7f894a7ddc890ba02c5f76d9468cb576889215ec
JAlexander94 <118635922+JAlexander94@users.noreply.github.com>
2023-01-20 19:59:47 +0000

    exercise 10 finished

--- activities/10-Stu-Data-Attributes/starter/assets/js/script.js
+++ activities/10-Stu-Data-Attributes/starter/assets/js/script.js
@@ -2,6 +2,16 @@ var container = document.querySelector(".container");
 
 container.addEventListener("click", function(event) {
   var element = event.target;
-
+  var state = element.getAttribute("data-state");
+  var number = element.getAttribute("data-number")
+  if(state ==="hidden"){
+    element.setAttribute("id",number)
+    document.getElementById(number).innerHTML = number;
+    element.setAttribute("data-state", "visible");
+  } else {
+    element.setAttribute("id",number)
+    document.getElementById(number).innerHTML = "";
+    element.setAttribute("data-state", "hidden");
+  }
   // TODO: Complete function
 });
